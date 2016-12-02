c38621fd9afb3a58566bd0f6c5a23144c358ae5d
leshayface <leshaywebdzn@gmail.com>
2016-12-02 11:22:19 +0200

    multiple imagesd

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,7 +6,7 @@ class Room < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :house_rules
 	has_and_belongs_to_many :room_amenities
-	has_many :room_images, :dependent => :destroy
+	has_many :room_images, validate: false
 
 	accepts_nested_attributes_for :location, :about_property, :about_room, :housemate, :house_rules, :room_amenities, :room_images, allow_destroy: true
 end
--- app/models/room_image.rb
+++ app/models/room_image.rb
@@ -1,6 +1,7 @@
 class RoomImage < ApplicationRecord
-	belongs_to :room
+	belongs_to :room, validate: false
 	has_attached_file :photo, :styles => { small: "100x100", med: "250x250", large: "500x500" }
 	validates_attachment_presence :photo
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 	validates_attachment_size :photo, :less_than => 5.megabytes
 end
