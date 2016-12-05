70091093faa64cf14d8cde3dee0f6c2385557de2
leshayface <leshaywebdzn@gmail.com>
2016-12-05 16:19:16 +0200

    Redirect to /rooms

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+   get '/rooms' => "welcome#index", as: :user_root
+  
+  
   root to: "home#index"
 end
